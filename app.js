37e588a0b7084a52dfc2c5912ae73a38a66b30eb
blackxavier <ogbuchiarinze01@gmail.com>
2024-07-01 17:58:20 +0100

    Unit testing 14

--- app.js
+++ app.js
@@ -1,3 +1,107 @@
+// const path = require('path');
+// const express = require('express');
+// const OS = require('os');
+// const bodyParser = require('body-parser');
+// const mongoose = require("mongoose");
+// const app = express();
+// const cors = require('cors')
+
+
+// app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, '/')));
+// app.use(cors())
+
+// // 
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://admin:admin@chiefbuilder.othg4tg.mongodb.net/?appName=chiefbuilder";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+// var Schema = mongoose.Schema;
+
+// var dataSchema = new Schema({
+//     name: String,
+//     id: Number,
+//     description: String,
+//     image: String,
+//     velocity: String,
+//     distance: String
+// });
+// var planetModel = mongoose.model('planets', dataSchema);
+
+
+
+// app.post('/planet',   function(req, res) {
+//    // console.log("Received Planet ID " + req.body.id)
+//     planetModel.findOne({
+//         id: req.body.id
+//     }, function(err, planetData) {
+//         if (err) {
+//             alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
+//             res.send("Error in Planet Data")
+//         } else {
+//             res.send(planetData);
+//         }
+//     })
+// })
+
+// app.get('/',   async (req, res) => {
+//     res.sendFile(path.join(__dirname, '/', 'index.html'));
+// });
+
+
+// app.get('/os',   function(req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send({
+//         "os": OS.hostname(),
+//         "env": process.env.NODE_ENV
+//     });
+// })
+
+// app.get('/live',   function(req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send({
+//         "status": "live"
+//     });
+// })
+
+// app.get('/ready',   function(req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send({
+//         "status": "ready"
+//     });
+// })
+
+// app.listen(3000, () => {
+//     console.log("Server successfully running on port - " +3000);
+// })
+
+
+// module.exports = app;
+
 const path = require('path');
 const express = require('express');
 const OS = require('os');
@@ -11,34 +115,18 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-// 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@chiefbuilder.othg4tg.mongodb.net/?appName=chiefbuilder";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function(err) {
+    if (err) {
+        console.log("error!! " + err)
+    } else {
+       console.log("MongoDB Connection Successful")
+    }
+})
 
 var Schema = mongoose.Schema;
 
